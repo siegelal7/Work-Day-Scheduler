4a5737512c18cb30e80cda81ae5ade1120be9a64
Andrew Siegel <siegelal7@gmail.com>
2020-09-10 22:12:30 -0400

    got it working..

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,37 +1,43 @@
 $(document).ready(function () {
   var calenderEl = $(".container");
   var currentDay = $("#currentDay");
+  //set header text to show current day
   var today = moment().format("dddd, MMMM, Do");
   currentDay.text(today);
-  var hours = ["9", "10", "11", "12", "13", "14", "16", "16", "17"];
-  var wholeList = [];
 
-  // var table = $("<table>");
-  // table.addClass("table");
-  // calenderEl.append(table);
-  // var tbody = $("<tbody>");
-  // tbody.addClass("time-block");
-  // table.append(tbody);
+  var hours = ["9", "10", "11", "12", "13", "14", "16", "16", "17"];
+  var wholeList = JSON.parse(localStorage.getItem("todoList")) || [];
 
   for (var i = 0; i < hours.length; i++) {
     var rows = $("<div>");
     rows.addClass("row");
     var day = $("<div>");
     day.addClass("col-2 time-block hour");
-
+    var dayText = "";
     if (hours[i] <= 11) {
-      day.text(`${hours[i]}AM`);
+      dayText = `${hours[i]}AM`;
+      // day.text(`${hours[i]}AM`);
     } else if (hours[i] == 12) {
-      day.text(`${hours[i]}PM`);
+      dayText = `${hours[i]}PM`;
+      // day.text(`${hours[i]}PM`);
     } else {
-      day.text(`${hours[i] - 12}PM`);
+      dayText = `${hours[i] - 12}PM`;
+      // day.text(`${hours[i] - 12}PM`);
     }
+    day.text(dayText);
     // day.text(hours[i]);
     var todo = $("<textarea>");
     todo.addClass("col-9 row text-column");
+    todo.attr("value", `${i}Text`);
+    // console.log(dayText);
+    // console.log(wholeList);
+    var filteredTasks = wholeList.filter((task) => task.time === dayText);
+    if (filteredTasks.length > 0) {
+      console.log(filteredTasks);
+      todo.val(filteredTasks[0].task);
+    }
 
     if (hours[i] == moment().hour()) {
-      // console.log(hours[i] - 12);
       todo.addClass("present");
     } else if (hours[i] < moment().hour()) {
       todo.addClass("past");
@@ -41,11 +47,19 @@ $(document).ready(function () {
 
     var btn = $("<button>");
     btn.attr("type", "submit");
+    btn.attr("value", `${i}Btn`);
     btn.addClass("saveBtn col-1");
     var icon = $("<i>");
     icon.addClass("fas fa-save");
     btn.append(icon);
-
+    btn.on("click", function () {
+      var saveObject = {
+        time: $(this).siblings("div").text(),
+        task: $(this).siblings("textarea").val(),
+      };
+      wholeList.push(saveObject);
+      localStorage.setItem("todoList", JSON.stringify(wholeList));
+    });
     rows.append(day);
     rows.append(todo);
     rows.append(btn);
@@ -53,9 +67,13 @@ $(document).ready(function () {
   }
 });
 
+// $(".saveBtn").on("click", function () {
+//   var saveObject = {
+//     time: $(this).siblings("div").text(),
+//     task: $(this).siblings("textarea").val(),
+//   };
+//   wholeList.push(saveObject);
+//   localStorage.setItem("todoList", JSON.stringify(wholeList));
+// });
 // var test = moment().hour();
-// if (test > 12) {
-//   console.log(test - 12);
-// } else {
-//   console.log(test);
-// }
+// console.log(test);
