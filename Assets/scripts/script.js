ca0169e912f2dfacb5ba86f592b10739e6007c73
Andrew Siegel <siegelal7@gmail.com>
2020-09-10 23:28:53 -0400

    updatign readme

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
   currentDay.text(today);
 
   var hours = ["9", "10", "11", "12", "13", "14", "16", "16", "17"];
+  // set this variable equal to either the stored value or empty so that the calender blocks populate
   var wholeList = JSON.parse(localStorage.getItem("todoList")) || [];
 
   for (var i = 0; i < hours.length; i++) {
@@ -14,29 +15,26 @@ $(document).ready(function () {
     var day = $("<div>");
     day.addClass("col-2 time-block hour");
     var dayText = "";
+    //assigning the AM or PM suffix
     if (hours[i] <= 11) {
       dayText = `${hours[i]}AM`;
-      // day.text(`${hours[i]}AM`);
     } else if (hours[i] == 12) {
       dayText = `${hours[i]}PM`;
-      // day.text(`${hours[i]}PM`);
     } else {
       dayText = `${hours[i] - 12}PM`;
-      // day.text(`${hours[i] - 12}PM`);
     }
     day.text(dayText);
-    // day.text(hours[i]);
+    //creating the text area of the todo list
     var todo = $("<textarea>");
     todo.addClass("col-9 row text-column");
-    todo.attr("value", `${i}Text`);
-    // console.log(dayText);
-    // console.log(wholeList);
+
+    // tries to find if any values are stored at that time, if so it creates an array with the elements that match the time
     var filteredTasks = wholeList.filter((task) => task.time === dayText);
     if (filteredTasks.length > 0) {
       console.log(filteredTasks);
       todo.val(filteredTasks[0].task);
     }
-
+    //sets the class of the todo task based on the time decided by moment.js
     if (hours[i] == moment().hour()) {
       todo.addClass("present");
     } else if (hours[i] < moment().hour()) {
@@ -52,8 +50,10 @@ $(document).ready(function () {
     var icon = $("<i>");
     icon.addClass("fas fa-save");
     btn.append(icon);
+    //had to have this event listener inside the loop in order for it to work
     btn.on("click", function () {
       var saveObject = {
+        //finds the value of each of the sibling elements in the generated html-set them as values in object to push to array then push to localStorage
         time: $(this).siblings("div").text(),
         task: $(this).siblings("textarea").val(),
       };
@@ -66,14 +66,3 @@ $(document).ready(function () {
     calenderEl.append(rows);
   }
 });
-
-// $(".saveBtn").on("click", function () {
-//   var saveObject = {
-//     time: $(this).siblings("div").text(),
-//     task: $(this).siblings("textarea").val(),
-//   };
-//   wholeList.push(saveObject);
-//   localStorage.setItem("todoList", JSON.stringify(wholeList));
-// });
-// var test = moment().hour();
-// console.log(test);
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Work-Day-Scheduler
+
+Welcome to your workday scheduler! Use this to plan out your day and ensure you are on time and on task.  
+The elements below the header are dynamically populated via Jquery, allowing more control over the designating classes based on time, etc. The biggest challenge was setting and retrieving the values to localStorage; discovering the siblings() method was pivotal in setting my object to push to the array. Ultimately, the biggest hint in creating and populating the page with the HTML was the provided CSS file, which had the textarea element hint and the class names assigned.
